ca8571ba42593bc2f4fdbb29556cb34652d41f59
marcus-daily <111281783+marcus-daily@users.noreply.github.com>
2024-08-21 17:22:35 +0100

    Custom body params

--- rtvi-client-android/src/main/java/ai/rtvi/client/utils/ConnectionBundle.kt
+++ rtvi-client-android/src/main/java/ai/rtvi/client/utils/ConnectionBundle.kt
@@ -1,10 +1,26 @@
 package ai.rtvi.client.utils
 
 import ai.rtvi.client.types.ServiceConfig
+import ai.rtvi.client.types.Value
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.encodeToJsonElement
+import kotlinx.serialization.json.jsonObject
 
 @Serializable
 internal data class ConnectionBundle(
     val services: Map<String, String>,
     val config: List<ServiceConfig>
-)
+) {
+
+    fun serializeWithCustomParams(customBodyParams: List<Pair<String, Value>>): String {
+
+        val customMap =
+            customBodyParams.associate { it.first to JSON_INSTANCE.encodeToJsonElement(it.second) }
+
+        val result = JsonObject(JSON_INSTANCE.encodeToJsonElement(this).jsonObject + customMap)
+
+        return JSON_INSTANCE.encodeToString(JsonObject.serializer(), result)
+    }
+}
